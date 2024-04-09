61fb649518d4a1f63c3f255fe43202680238cda8
VitorSakuma <vitorsakuma85@gmail.com>
2024-04-09 16:59:34 -0300

    at

--- Atividade Aula6- TUPLAS/desafio3.py
+++ Atividade Aula6- TUPLAS/desafio3.py
@@ -11,3 +11,10 @@
 # C) Uma lista com os times em ordem alfabética.
 
 # D) Em que posição na tabela está o time do Santos.
+
+
+
+time = ("Amazonas FC" , "America MG" , "Avai" , "Botafogo SP" , "Brusque", "CRB" , "Ceara SC" , "Chapecoense" , "Coritiba" , "Goias" , "Guarani" , "Ituano" , "Mirassol" , "Novorizontino" , "Operario" , "Paysandu" , "Ponte Preta" , "Santos" , "Sport Recife" , "Vila Nova")
+
+print(f"lista dos times brasileiros serie B: {time}")
+print(f)
