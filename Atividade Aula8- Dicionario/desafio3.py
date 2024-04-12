f490205eada28a69f651a33794b831414b70aba3
VitorSakuma <vitorsakuma85@gmail.com>
2024-04-12 16:54:15 -0300

    salvo

--- Atividade Aula8- Dicionario/desafio3.py
+++ Atividade Aula8- Dicionario/desafio3.py
@@ -10,3 +10,39 @@
 # Sabendo que ele vai se aposentar após 35 anos de
 # colaboração.
 
+from datetime import date
+
+anoAtual = date.today().year
+
+dicionario = {
+    "nome" : str(input("Nome: ")),
+    "Ano de Nascimento" : int(input("Ano de Nascimento: ")),
+    "Carteira de Trabalho" : int(input("Carteira de trabalho (0 se não tiver: "))
+}
+
+print(dicionario)
+
+idade = anoAtual - dicionario["Ano de Nascimento"]
+del dicionario ["Ano de Nascimento"]
+dicionario ["idade"] = idade
+
+print(dicionario)
+
+if dicionario["Carteira de Trabalho"]==0:
+    for k, v in dicionario.items():
+        print(f" {k} = {v}")
+        
+else:
+    dicionario["Ano Contratacao"] = int(input("Ano de  Contratação: "))
+    dicionario["Salario"] = float(input("Salario: "))
+    tempoTrabalho = anoAtual - dicionario["Ano Contratacao"]
+    if tempoTrabalho > 35:
+        dicionario["Aposentadoria"] = "aposentado"
+    
+    else:
+        tempoAposentadoria = idade + (35 - tempoTrabalho)
+        dicionario["Aposentadoria"] = tempoAposentadoria
+        for k, v in dicionario.items():
+            print(f" {k} tem o valor {v}")
+        
+    
