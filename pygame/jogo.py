fa7cefaefb3d5e6d595479295d6573afe927a888
VitorSakuma <vitorsakuma85@gmail.com>
2024-04-23 17:03:18 -0300

    salvofinal

---
+++ pygame/jogo.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.locals import *
+
+from sys import exit
+from random import *
+pygame.init()
+
+altura = 480
+largura = 640
+
+#Definindo posição e tamanho do objeto
+pos_y_retangulo = altura / 2
+pos_x_retangulo = largura / 2
+largura_retangulo = 30
+altura_retangulo = 40
+
+#Definindo posiçãoe tamanha do Circulo
+
+pos_y_circulo = 40
+pos_x_circulo = 40
+raio_circulo = 10
+
+#definindo fonte
+
+fonte = pygame.font.SysFont("Arial", 20, True, False)
+pontos = 0
+
+#Criando a janela
+tela = pygame.display.set_mode((largura, altura))
+
+#Titulo da janela
+pygame.display.set_caption("Criando Jogos")
+#nodificar a taxa de atualização de Pixels/segundo
+relogio = pygame.time.Clock()
+
+while True:
+    relogio.tick(30)
+    tela.fill((0,0,0))
+    mensagem = f"pontos: {pontos}"
+    textoFormatado = fonte.render(mensagem, True, (255,255,255))
+    
+    #Colocando os eventos no Pygame
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        
+        # if event.type == KEYDOWN:
+        #     if event.key == K_UP:
+        #         pos_y_retangulo -= 10
+        #     elif event.key == K_DOWN:
+        #         pos_y_retangulo += 10
+        #     elif event.key == K_LEFT:
+        #         pos_x_retangulo -= 10
+        #     elif event.key == K_RIGHT:
+        #         pos_x_retangulo += 10
+    if pygame.key.get_pressed()[K_UP]:
+        pos_y_retangulo -= 10
+    elif pygame.key.get_pressed()[K_DOWN]:
+        pos_y_retangulo += 10
+    elif pygame.key.get_pressed()[K_LEFT]:
+        pos_x_retangulo -= 10
+    elif pygame.key.get_pressed()[K_RIGHT]:
+        pos_x_retangulo += 10
+        
+     
+    retangulo = pygame.draw.rect(tela, (0,0,255), (pos_x_retangulo,pos_y_retangulo,largura_retangulo,altura_retangulo)) 
+    circulo = pygame.draw.circle(tela, (255,0,255), (pos_x_circulo,pos_y_circulo), raio_circulo)
+    
+    if retangulo .colliderect(circulo):
+        pos_x_circulo = randint(40,600)
+        pos_y_circulo = randint(50,430)
+        pontos += 1
+        
+    tela.blit(textoFormatado, (400,40))
+        
+        
+    #Atualizar o jogo a toda interação
+    pygame.display.update()
